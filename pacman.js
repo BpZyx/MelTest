cb18473d1423e136a18415deb279ef7fc4d26ccd
BpZyx <130698924+BpZyx@users.noreply.github.com>
2023-07-29 02:53:03 +0700

    Update pacman.js

--- pacman.js
+++ pacman.js
@@ -112,3 +112,53 @@ canvas.addEventListener('touchend', (event) => {
 canvas.addEventListener('touchmove', (event) => {
   event.preventDefault();
 });
+// ... Existing code ...
+
+// Touch event handling for mobile
+let touchStartX = 0;
+let touchStartY = 0;
+
+canvas.addEventListener('touchstart', (event) => {
+  event.preventDefault();
+  touchStartX = event.touches[0].clientX;
+  touchStartY = event.touches[0].clientY;
+});
+
+canvas.addEventListener('touchend', (event) => {
+  event.preventDefault();
+  const touchEndX = event.changedTouches[0].clientX;
+  const touchEndY = event.changedTouches[0].clientY;
+
+  const deltaX = touchEndX - touchStartX;
+  const deltaY = touchEndY - touchStartY;
+
+  if (Math.abs(deltaX) > Math.abs(deltaY)) {
+    // Horizontal swipe
+    if (deltaX > 0) {
+      keys[39] = true; // Right arrow key
+      keys[37] = false; // Reset left arrow key
+    } else {
+      keys[37] = true; // Left arrow key
+      keys[39] = false; // Reset right arrow key
+    }
+    keys[38] = false; // Reset up arrow key
+    keys[40] = false; // Reset down arrow key
+  } else {
+    // Vertical swipe
+    if (deltaY > 0) {
+      keys[40] = true; // Down arrow key
+      keys[38] = false; // Reset up arrow key
+    } else {
+      keys[38] = true; // Up arrow key
+      keys[40] = false; // Reset down arrow key
+    }
+    keys[37] = false; // Reset left arrow key
+    keys[39] = false; // Reset right arrow key
+  }
+});
+
+canvas.addEventListener('touchmove', (event) => {
+  event.preventDefault();
+});
+
+// ... Continue with the rest of the code ...
